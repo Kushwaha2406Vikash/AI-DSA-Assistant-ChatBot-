701bc0aa4d083d0d2139d5150e1660aa7f24173f
Vikash Kushwaha <136932881+Kushwaha2406Vikash@users.noreply.github.com>
2025-08-16 17:03:32 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,17 +6,20 @@ import chatRoute from './routes/chatRoute.js'
 dotenv.config();
 
 const app = express();
-
-app.use(cors({
-  origin: "*",
-  credentials: true, 
-}));
+const PORT = process.env.PORT || 8080;
+// app.use(cors({
+//   origin: "*",
+//   credentials: true, 
+// }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({ origin: "https://ai-dsa-assistant-chat-bot.vercel.app", credentials: true, }));
+
 
 
 app.use("/api",chatRoute);
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log("port is listening 3000 port");
 })
