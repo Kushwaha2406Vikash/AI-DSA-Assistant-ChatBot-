d778e8779739b0f9cedd9d262fe9203b234ed934
Vikash Kushwaha <136932881+Kushwaha2406Vikash@users.noreply.github.com>
2025-08-21 23:55:37 +0530

    Update cors policy

--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,17 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-// app.use(cors({
-//   origin: "*",
-//   credentials: true, 
-// }));
+
+app.use(cors({ 
+    origin: "https://ai-dsa-assistant-chat-bot.vercel.app",
+    methods:["GET","POST","PUT","DELETE"],
+    credentials: true, 
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({ origin: "https://ai-dsa-assistant-chat-bot.vercel.app", credentials: true, }));
+//app.use(cors({ origin: "https://ai-dsa-assistant-chat-bot.vercel.app", credentials: true, }));
 
 
 
